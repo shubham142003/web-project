8779d8a0aef742153c7cea230284bbed0195f5c9
shubh <100335234+shubham142003@users.noreply.github.com>
2022-07-27 22:54:46 +0530

    Update rating.js

--- rating.js
+++ rating.js
@@ -1,6 +1,6 @@
 var content = document.getElementById("thank");
 let text = `<div class="thankb slide-in-elliptic-top-fwd">
-  <img src="/illustration-thank-you.svg" alt="" class="thanku">
+  <img src="illustration-thank-you.svg" alt="" class="thanku">
   <span><p>You Selected 4 out 5</p></span>
   <h1>Thank You</h1>
   <p>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in
